517053cffad11ab888a3d7324ee1de3afd6c3fb3
Prathamesh Thakur <84024452+prathamesh-webdesigner@users.noreply.github.com>
2025-01-18 22:59:49 +0530

    shampoo section animation

--- js/script.js
+++ js/script.js
@@ -1,53 +1,124 @@
 // Text Animation 
 function breakTheText() {
-    var h1 = document.querySelector(".bannerTextArea h1");
-    var h1Text = h1.textContent;
+    var elements = document.querySelectorAll(".bannerTextArea h1, .bestfriend h2, .bestfriend h3");
 
-    var splittedText = h1Text.split("");
-    var cultter = "";
+    elements.forEach(function(element) {
+        var text = element.textContent;
+        var splittedText = text.split("");
+        var clutter = "";
 
-    splittedText.forEach(function (elem) {
-        cultter += `<span>${elem}</span>`;
-    })
+        // Loop through each character and wrap them in <span>
+        splittedText.forEach(function(char) {
+            // Check if the character is a space and use &nbsp; instead
+            if (char === " ") {
+                clutter += `<span>&nbsp;</span>`;
+            } else {
+                clutter += `<span>${char}</span>`;
+            }
+        });
 
-    h1.innerHTML = cultter;
+        element.innerHTML = clutter;
+    });
 }
 
-breakTheText()
+breakTheText();
+
+
+// Call the function
+breakTheText();
+
 
 
 gsap.from(".bannerTextArea h1 span", {
     y: 70,
     opacity: 0,
     duration: 0.5,
-    delay: 0.5,
-    stagger: 0.1
+    delay: 1.5,
+    stagger: 0.1,
 })
-// gsap.from("h2 span",  {
-//     y:70,
-//     opacity:0,
-//     duration:0.5,
-//     delay:0.5,
-//     stagger: 0.03
-// })
-gsap.from(".sayAni", {
+
+gsap.from(".bannerTextArea p, .bannerTextArea h2, .sayAni, .mo_goodbye", {
     x: -70,
     opacity: 0,
     duration: 1,
-    delay: 0.9,
-})
-gsap.from(".bannerTextArea p, .bannerTextArea h2", {
-    x: -70,
-    opacity: 0,
-    duration: 1,
-    delay: 0.9,
+    delay: 1.5,
 })
 gsap.from(".bannerBoy img", {
     x: 70,
     opacity: 0,
     duration: 1,
-    delay: 0.9,
+    delay: 1.5,
 })
+
+// ScrollTrigger 
+gsap.from(".bestfriend h2 span, .bestfriend h3 span", {
+    y: 70,
+    opacity: 0,
+    duration: 0.5,
+    delay: 0.8,
+    stagger: 0.1,
+    scrollTrigger: ".bestfriend"
+})
+// Shampoo
+gsap.from(".sayGoodbyeParent", {
+    y:"100%",
+    duration: 1,
+    delay:2,
+    scrollTrigger: ".bestfriend"
+})
+gsap.from(".shampoo_image", {
+    y:"100%",
+    x:"80%",
+    duration: 1,
+    delay:3,
+    scrollTrigger: ".bestfriend"
+})
+// Shampoo end
+// Daily Care
+gsap.from(".gentleEnoughParent", {
+    y:"100%",
+    duration: 1,
+    delay:2.2,
+    scrollTrigger: ".bestfriend"
+})
+gsap.from(".gentleEnoughImage", {
+    y:"100%",
+    duration: 1,
+    delay:3.2,
+    scrollTrigger: ".bestfriend"
+})
+// Daily care end
+// Care
+gsap.from(".hydrateParent", {
+    y:"100%",
+    duration: 1,
+    delay:2.4,
+    scrollTrigger: ".bestfriend"
+})
+gsap.from(".hydrateImage", {
+    y:"100%",
+    x:"-80%",
+    duration: 1,
+    delay:3.4,
+    scrollTrigger: ".bestfriend"
+})
+// Care end
+// Round Feature 
+gsap.from(".round_featureParent",{
+    scale:0,
+    duration: 1,
+    delay:4,
+    scrollTrigger: ".bestfriend"
+})
+// Round Feature End
+gsap.from(".yourScalpsBottomParent",{
+    y:50,
+    opacity:0.8,     // Start fully transparent
+    duration:1,
+    delay:4,
+    scrollTrigger: ".bestfriend"
+})
+// ScrollTrigger End
 
 // Blink Animation 
 // Remove the blinking element after 2 seconds
