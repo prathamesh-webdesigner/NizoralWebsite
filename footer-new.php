fe339af27a3b98413cab515893e654286daf15a2
Bhagyesh Kambli <bhagyesh@brandmovers.in>
2025-02-06 00:28:40 +0530

    Home banner animation WIP

---
+++ footer-new.php
@@ -0,0 +1,73 @@
+<footer class="scrollSection">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 text-center text-md-start">
+                <img src="image/Nizoral.png" class="mb-5" alt="Nizoral">
+            </div>
+            <div class="col-md-3 col-6">
+                <h3>Useful Links </h3>
+                <ul>
+                    <li><a href="dandruff-education.php">Dandruff Education</a></li>
+                    <li><a href="nizoral-range.php">Nizoral Range</a></li>
+                    <li><a href="#">Scalp & Hair Hub</a></li>
+                    <li><a href="#">Scalp Quiz</a></li>
+                    <li><a href="#">Where to Buy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3 col-6">
+                <h3>Quick Links</h3>
+                <ul>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Terms of Use</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <img src="image/stada.webp" class="stada" alt="Stada">
+                <ul>
+                    <li><a href="#">&#169;Stada 2025</a></li>
+                    <li><a href="#">email@example.com</a></li>
+                    <li><a href="#">002-010-66269735</a></li>
+                </ul>
+                <ul class="social_icon">
+                    <li><a href="#"> <img src="image/facebook.svg" alt="facebook"> </a></li>
+                    <li><a href="#"> <img src="image/youtube.svg" alt="youtube"> </a></li>
+                    <li><a href="#"> <img src="image/instagram.svg" alt="instagram"> </a></li>
+                    <li><a href="#"> <img src="image/tiktok.svg" alt="tiktok"> </a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- Snap Scroll -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/scrollify/1.0.21/jquery.scrollify.min.js" integrity="sha512-UyX8JsMsNRW1cYl8BoxpcamonpwU2y7mSTsN0Z52plp7oKo1u92Xqfpv6lOlTyH3yiMXK+gU1jw0DVCsPTfKew==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Snap Scroll End  -->
+<!-- Gsap  -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js"
+    integrity="sha512-7eHRwcbYkK4d9g/6tD/mhkf++eoTHwpNM9woBxtPUBWm67zeAfFC+HrdoE2GanKeocly/VxeLvIqwvCdk7qScg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js"
+    integrity="sha512-onMTRKJBKz8M1TnqqDuGBlowlH0ohFzMXYRNebz+yOcc5TQr/zAKsthzhuv0hiyUKEiQEQXEynnXCvNTOk50dg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- Gsap  end-->
+
+<!-- owl carousel -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"
+    integrity="sha512-bPs7Ae6pVvhOSiIcyUClR7/q2OAsRiovw4vAkX+zJbw3ShAeeqezq50RIIcIURq7Oa20rW2n2q+fyXBNcU9lrw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!-- owl carousel  -->
+
+<script src="js/script-new.js?v=8"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ js/script-new.js
@@ -0,0 +1,416 @@
+// Header visible to invisible start
+document.onreadystatechange = function () {
+    let lastScrollPosition = 0;
+    const navbar = document.querySelector('header');
+    window.addEventListener('scroll', function (e) {
+        lastScrollPosition = window.scrollY;
+
+        if (lastScrollPosition > 100)
+            navbar.classList.add('hide-header');
+        else
+            navbar.classList.remove('hide-header');
+    });
+}
+
+// Header visible to invisible end
+
+// owl carousel 
+// Owl Carosel
+$('.homeSlider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayTimeout: 5000,
+    autoplayHoverPause:true,
+    navText: [
+        '<img src="image/prev-thin.png" alt="Previous" style="width:40px;">',
+        '<img src="image/next-thin.png" alt="Next" style="width:40px;">'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+});
+$('.nizoral-product').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    dots: false,
+    autoplay: true,
+    smartSpeed: 1000,
+    autoplayTimeout: 3000,
+    autoplayHoverPause:true,
+    navText: [
+        '<img src="image/prev.png" alt="Previous" style="width:40px;">',
+        '<img src="image/next.png" alt="Next" style="width:40px;">'
+    ],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
+// owl carousel end 
+
+function initBanner() { 
+    // Banner	
+    let tl_banner = gsap.timeline({
+        scrollTrigger:{
+            trigger: ".banner",
+            scrub: false,
+            pin: false,
+        }
+    });		
+    tl_banner
+        .fromTo(".sayAni, .bannerTextArea p, .md_goodbye, .md_dandruffAndItching, .bannerBoy img",
+            {
+                opacity: 0,
+                filter: "blur(3px)",
+                clipPath: "inset(0 100% 0 0)", 
+            } ,
+            {
+                filter: "blur(0px)",  // Removes blur gradually
+                clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+                duration: 2,
+                opacity: 1,
+                ease: "power2.out"
+            }
+        )
+    ;
+        	
+    
+}
+initBanner();
+/* 
+gsap.to(".sayAni, .bannerTextArea p, .md_goodbye, .md_dandruffAndItching, .bannerBoy img", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 2,
+    delay: 1.5,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out"
+});
+
+
+gsap.to(".bannerBoy img", {
+    x: -50,
+    duration: 1,
+    delay: 1.4,
+    opacity: 1,
+    ease: "power2.out"
+});
+
+gsap.to(".bestfriend h2, .bestfriend h3", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 1.5,
+    delay: 1.5,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out",
+    scrollTrigger: ".bestfriend"
+});
+
+
+// ScrollTrigger 
+
+// Shampoo
+gsap.from(".sayGoodbyeParent", {
+    // y:"100%",
+    clipPath: "inset(100% 0 0 0)",
+    duration: 1,
+    delay: 2,
+    scrollTrigger: ".bestfriend"
+})
+gsap.from(".shampoo_image", {
+    y: "100%",
+    x: "80%",
+    duration: 1,
+    delay: 3,
+    scrollTrigger: ".bestfriend"
+})
+// Shampoo end
+// Daily Care
+gsap.from(".gentleEnoughParent", {
+    clipPath: "inset(100% 0 0 0)",
+    duration: 1,
+    delay: 2.2,
+    scrollTrigger: ".bestfriend"
+})
+gsap.from(".gentleEnoughImage", {
+    y: "100%",
+    duration: 1,
+    delay: 3.2,
+    scrollTrigger: ".bestfriend"
+})
+// Daily care end
+// Care
+gsap.from(".hydrateParent", {
+    clipPath: "inset(100% 0 0 0)",
+    duration: 1,
+    delay: 2.4,
+    scrollTrigger: ".bestfriend"
+})
+gsap.from(".hydrateImage", {
+    y: "100%",
+    x: "-80%",
+    duration: 1,
+    delay: 3.4,
+    scrollTrigger: ".bestfriend"
+})
+// Care end
+// Round Feature 
+gsap.from(".round_featureParent", {
+    scale: 0,
+    duration: 1,
+    delay: 4,
+    scrollTrigger: ".bestfriend"
+})
+// Round Feature End
+gsap.from(".yourScalpsBottomParent", {
+    clipPath: "inset(70% 0 0 0)",
+    duration: 1,
+    delay: 1,
+    scrollTrigger: ".bestfriend"
+}) */
+
+// ----------------- Top banner ends---------------------
+
+gsap.to(".theNezroralDiffrenceParent h2", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 1.5,
+    delay: 0.5,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out",
+    scrollTrigger: ".theNezroralDiffrenceParent_main"
+});
+
+gsap.from(".round_icon_area, .round_icon_area_for_clinically_proven, .fast_relief_round_icon_area, .triple_action_round_icon_area", {
+    scale: 0,
+    duration: 1,
+    delay: 1,
+    scrollTrigger: ".slider_first"
+})
+gsap.to(".allAboutDandruffImage img", {
+    top: "-15%",
+    duration: 1.5,
+    delay: 0,
+    scrollTrigger: {
+        trigger: ".allAboutDandruffParent",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+})
+gsap.to(".hairCareRoutineHeading h2", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 1.5,
+    delay: 1,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out",
+    scrollTrigger: "#hairCareRoutine"
+});
+// treat_parent
+gsap.from(".treat_hed", {
+    y: -100, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: ".treat_parent"
+});
+gsap.from(".shampoo_drop", {
+    x: -200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: ".treat_parent"
+});
+gsap.from(".treat_shampoo_image", {
+    x: 200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: ".treat_parent"
+});
+gsap.to(".treat_text p", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 1.5,
+    delay: 0.5,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out",
+    scrollTrigger: ".treat_parent"
+});
+// Prevent 
+gsap.from(".prevent_area h2", {
+    y: -100, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: {
+        trigger: ".prevent",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+gsap.from(".daily_care_product", {
+    x: -200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: {
+        trigger: ".prevent",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+gsap.from(".blue-drop", {
+    x: 200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: {
+        trigger: ".prevent",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+gsap.to(".daily_care_text p", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 1.5,
+    delay: 0.5,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".prevent",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+// Restore
+gsap.from(".restore_area h2", {
+    y: -100, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: {
+        trigger: ".restore",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+gsap.from(".restore_product", {
+    x: 200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: {
+        trigger: ".restore",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+gsap.from(".restore-drop", {
+    x: -200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: {
+        trigger: ".restore",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+gsap.to(".restore_para p", {
+    filter: "blur(0px)",  // Removes blur gradually
+    clipPath: "inset(0 0% 0 0)", // Reveals text from left to right
+    duration: 1.5,
+    delay: 0.5,
+    filter: "blur(0px)",
+    opacity: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".restore",
+        scroller: "body",
+        start: "top 50",
+        end: "top 50%",
+        scrub: false,
+        markers: false,
+    }
+});
+// Expert Advice 
+gsap.from(".expret_advice_wrapper", {
+    y: 200, // Moves text up before revealing
+    opacity: 0, // Starts with invisible text
+    duration: 1.5,
+    delay: 0.5,
+    ease: "power2.out", // Smooth easing effect
+    scrollTrigger: ".expret_advice"
+});
+// ScrollTrigger End
+
+// Blink Animation 
+// Remove the blinking element after 2 seconds
+setTimeout(() => {
+    const blinkText = document.getElementById('blinkText');
+    blinkText.style.display = 'none';
+}, 1000);
+
+
+
