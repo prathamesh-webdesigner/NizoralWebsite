e0814dbd7936c21c27946fc59170f114fb5e13ac
Kalpesh <kalpesh@brandmovers.in>
2025-02-06 21:06:52 +0530

    php page converted

--- footer.php
+++ footer.php
@@ -63,6 +63,7 @@
 <!-- owl carousel  -->
 
 <script src="js/script.js?v=8"></script>
+<script src="js/scalp-hair-hub.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
