328e3354bdf84da6028ebe6b3fa033f3e7177f2b
unknown <thakurpratham15@gmail.com>
2025-02-07 12:34:30 +0530

    CSS and JS version change for clear cache

--- header.php
+++ header.php
@@ -17,9 +17,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="css/style.css?v=9">
-    <link rel="stylesheet" href="css/media.css?v=9">
-    <link rel="stylesheet" href="css/inner-pages.css">
+    <link rel="stylesheet" href="css/style.css?v=10">
+    <link rel="stylesheet" href="css/media.css?v=10">
+    <link rel="stylesheet" href="css/inner-pages.css?v=10">
 
     <!-- <style>
         html,
