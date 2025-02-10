e489091bdc92409d9864550764d00b375944efd6
Prathamesh Thakur <84024452+prathamesh-webdesigner@users.noreply.github.com>
2025-02-11 01:58:34 +0530

    Dandruff fungi structure created

--- dandruff-fungi.php
+++ dandruff-fungi.php
@@ -26,11 +26,163 @@
             <p class="body-sm">The causes of dandruff are manifold. Sensitive scalp may react to hair products, nutrition or hormonal
                 and seasonal changes. However, various skin diseases can also be the cause.</p>
             <ul>
-                <li class="body-md"><a href="#">Dandruff</a></li>
-                <li class="body-md"><a href="#">Fungus</a></li>
-                <li class="body-md"><a href="#">Dermatitis</a></li>
-                <li class="body-md"><a href="#">Skin</a></li>
+                <li class="body-md"><a href="#Dandruff">Dandruff</a></li>
+                <li class="body-md"><a href="#Fungus">Fungus</a></li>
+                <li class="body-md"><a href="#Dermatitis">Dermatitis</a></li>
+                <li class="body-md"><a href="#Skin">Skin</a></li>
             </ul>
+        </div>
+    </div>
+</section>
+
+<section class="air_pollution h-auto" id="Dandruff">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 p-0">
+                <div class="air_pollution_image">
+                    <img src="image/flakyScalp.webp" alt="Dandruff">
+                </div>
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+                <div class="air_pollution_content">
+                    <h2 class="font-bold heading-mlg">Dandruff</h2>
+                    <h3 class="body-lg font-bold">What is "normal" dandruff?</h3>
+                    <P class="body-sm">Dandruff is a condition of the scalp that causes visible scales, consisting of dead skin cells that clump together with the sebum of the skin to form white to yellowish flakes. Sebaceous glands are present in the skin of every human being. These glands produce lipids. When the lipids reach the surface of the skin, they are called sebum (a mixture of fats and proteins).
+                    </P>
+                    <p class="body-sm">Sebum makes hair and skin smooth, shiny and water repellent. It protects against dehydration. Dandruff can also be found on the scalp of almost every person, because our skin is constantly renewing itself by shedding old, dead skin cells. When it comes into contact with sebum, it becomes visible as skin flakes.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="extream_weather h-auto" id="Fungus">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-7 order-2 order-md-1 d-flex align-items-center">
+                <div class="air_pollution_content">
+                    <h2 class="heading-mlg font-bold">Fungus</h2>
+                    <h3 class="font-bold body-lg">Malassezia furfur</h3>
+                    <P class="body-sm">Malassezia is a genus of fungi (yeast-like fungi). They naturally occur on the skin surface of humans and animals. One of these fungi species is Malassezia furfur. The fungus needs fat to grow. Therefore, it likes to live on sebaceous skin and hairy areas of the body:</p>
+                    <div class="light_yellow_area">
+                        <p class="body-md font-bold">
+                            Scalp <br>
+                            Facial skin <br>
+                            Chest and back</p>
+                    </div>
+                    <p>A concentration of up to 46% of the fungus on the scalp is completely normal and not harmful. However, if the fungus grows out of control (up to 74%), it can trigger dandruff. At more than 83%, it can cause skin diseases.</p>
+                </div>
+
+            </div>
+            <div class="col-md-5 order-1 order-md-2 p-0">
+                <div class="air_pollution_image">
+                    <img src="image/dandruff-fungi/fungus.webp" alt="Fungus">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="air_pollution h-auto" id="Dermatitis">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 p-0">
+                <div class="air_pollution_image">
+                    <img src="image/dandruff-fungi/Dermatitis.webp" alt="Dermatitis">
+                </div>
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+                <div class="air_pollution_content">
+                    <h2 class="font-bold heading-mlg">Dermatitis</h2>
+                    <h3 class="body-lg font-bold">Seborrheic dermatitis</h3>
+                    <P class="body-sm font-bold">Seborrheic dermatitis occurs when there is a severe skin infestation with the yeast-like fungus Malassezia furfur. It is also called seborrheic eczema. The good news: seborrheic dermatitis is not contagious.
+                    </P>
+                    <p class="body-sm">As much as 50% of the world's population suffers from dandruff. It is one of the most common skin diseases worldwide. Dandruff and seborrheic dermatitis can be very burdening for those affected. And it has to be treated with the right means.
+                    </p>
+                    <p class="body-sm">Seborrheic dermatitis and dandruff often occur on the head and face, but it can also affect other parts of the body. It is often seasonal: those affected suffer more during winter than summer. Antifungal agents are recommended for therapy.
+                    </p>
+                    <p class="body-sm">How to recognize seborrheic dermatitis?
+                    </p>
+                    <div class="light_yellow_area">
+                        <p class="body-md font-bold">
+                            Clear crusts that appear yellowish and greasy <br>
+                            Sometimes the skin is reddish in the affected areas <br>
+                            The skin often burns and/or itches there</p>
+                    </div>
+                    <p class="body-sm">However, there are other skin disorders that look similar, but have other causes. These include psoriasis or neurodermatitis (atopic dermatitis). For a correct diagnosis, always ask a dermatologist.</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="extream_weather h-auto" id="Skin">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-7 order-2 order-md-1 d-flex align-items-center">
+                <div class="air_pollution_content">
+                    <h2 class="heading-mlg font-bold">Skin</h2>
+                    <h3 class="font-bold body-lg">Tinea Versicolor (Pityriasis versicolor)</h3>
+                    <P class="body-sm">The yeast-like fungus Malassezia is the causative agent of tinea versicolor (pityriasis versicolor). The most common cause is Malassezia furfur. Common symptoms of the disease are:</p>
+                    <div class="light_yellow_area">
+                        <p class="body-md font-bold">
+                            Initially small, later larger, round to oval spots on the skin. <br>
+                            Spots in different shades of red (pink, brown, red, black).<br>
+                            Affected areas of the skin are significantly lighter than the surrounding healthy skin or even without pigmentation (pityriasis versicolor alba).<br>
+                            The skin is slightly swollen in the affected areas, but itching occurs rather rarely and if so, only slightly.
+                        </p>
+                    </div>
+                    <p class="body-sm">Do not scratch. Otherwise, the skin may shed in small flakes. The bran fungus does not cause dandruff-like seborrheic dermatitis, but it is also caused by the yeast-like fungus Malassezia furfur.
+                    </p>
+                    <p class="body-sm">What helps? Antifungal agents are particularly successful in acute therapy. However, the spots on the affected areas are often visible for several months after treatment. Treatment should be continued as a precautionary measure, even after healing.</p>
+                </div>
+
+            </div>
+            <div class="col-md-5 order-1 order-md-2 p-0">
+                <div class="air_pollution_image">
+                    <img src="image/dandruff-fungi/Skin.webp" alt="Skin">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="antifungalParent">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 d-flex align-items-center">
+                <div class="antifungal_agent">
+                    <h2 class="heading-md font-bold">How do antifungal
+                        agents work?</h2>
+                    <p>Antifungals eliminate the cause of diseases that are originated by fungi. If the scalp is excessively colonized with the yeast-like fungus Malassezia furfur, the active ingredient Ketoconazole 2% has proven to be particularly suitable. It damages the cell membrane of the fungus. Thus, Ketoconazole 2% fights its excessive growth and minimizes the formation of dandruff. The effect lasts for up to two weeks after rinsing.</p>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div class="antifungal_shampoo d-flex align-items-end gap-4">
+                    <img src="image/slider-shampoo.webp" alt="Shampoo">
+                    <div>
+                        <h2 class="body-md font-bold">Nizoral® <br>
+                            2% Medical Shampoo</h2>
+                        <p class="body-sm mb-4">Fast, Effective Dandruff Treatment</p>
+                        <p class="body-sm">Clinically proven to relieve dandruff and itching from Day 1 and prevent recurrence for up to 3 months.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="info_fungi">
+    <div class="container">
+        <div class="row">
+            <div class="infoArea">
+                <h2 class="display-mlg font-bold">INFO</h2>
+                <p class="body-sm">Antifungals are medications used to combat the growth of fungi. Antifungals consist of antimicrobial substances that prevent fungi from growing or that can kill fungi. Therefore, antifungals are used to effectively treat fungal diseases (mycoses).
+                </p>
+                <p class="body-sm">Antimicrobials are chemicals that reduce or kill microorganisms. In addition to antifungals, this includes antibiotics to fight bacteria as well as all disinfectants.</p>
+            </div>
         </div>
     </div>
 </section>
